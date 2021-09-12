c22941c369b9abb70fab4fb0cea7945cdfce733b
David Low <david.noyes.low@gmail.com>
2021-09-12 03:56:35 -0400

    removed unneccessary parenthesis

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var axios = require("axios");
 // Establish bitcoin cash seed phrase
 var mnemonic = new bcash.Mnemonic("vacant shiver similar power oil real riot food field grow mind wet");
 // Convert seed phrase to String
-var seedPhrase = (mnemonic.toString());
+var seedPhrase = mnemonic.toString();
 // convert phrase string to Array
 var seedArray = seedPhrase.split(" ");
 // convert seed array back to String while removing commas
 var reSeed = seedArray.toString(" ").replace(/\,/g, " ");
 
-console.log(reSeed);
+console.log(seedPhrase);
 
 
 
