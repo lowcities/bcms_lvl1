54474234c0a6e0e7fe00260f1c2e81e1c4f35eb6
David Low <david.noyes.low@gmail.com>
2021-09-11 23:42:05 -0400

    added comments

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var axios = require("axios");
 
 // Establish bitcoin cash seed phrase
 var mnemonic = new bcash.Mnemonic("vacant shiver similar power oil real riot food field grow mind wet");
+// Convert seed phrase to String
 var seedPhrase = (mnemonic.toString());
+// convert phrase string to Array
 var seedArray = seedPhrase.split(" ");
+// convert seed array back to String while removing commas
 var reSeed = seedArray.toString(" ").replace(/\,/g, " ");
 
 console.log(reSeed);
