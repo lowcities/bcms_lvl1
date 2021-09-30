80ea2703b519311c0d15757a69d1d26b58e342a2
David Low <david.noyes.low@gmail.com>
2021-09-29 20:21:58 -0400

    added address deriving methof and function to create an object of address'

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var bcash = require("bcash");
 var axios = require("axios");
+const {WalletClient, Network, WalletDB, KeyRing} = require('bcash');
+// Establish BCH network
+const network = Network.get('main');
 
 
 
@@ -9,39 +12,56 @@ var axios = require("axios");
 // Establish bitcoin cash seed phrase
 var mnemonic = new bcash.Mnemonic("vacant shiver similar power oil real riot food field grow mind wet");
 
+
 // Convert seed phrase to String
 var seedPhrase = mnemonic.toString();
 // convert phrase string to Array
-// var phraseArray = seedPhrase.split(" ");
+var phraseArray = seedPhrase.split(" ");
 // // convert seed array back to String while removing commas
-// var rePhrase = phraseArray.join(' ');
+var rePhrase = phraseArray.join(' ');
 
 // // console.log("Array: ", phraseArray);
-// var firstLetterArray = [];
-// var letterObject = { totalLetters: 0 };
 
-// for(let i = 0; i < phraseArray.length; i++) {
-//     let word = phraseArray[i]; //word will equal the current word in the iteration
-//     firstLetterArray.push(word[0]); //pushes the first letter of each word to an array
-//      console.log('word ' + i, word);
-//      letterObject.totalLetters = letterObject.totalLetters + word.length; //totals all letters in all words and stores it to letterObject totalLetters key/val pair
-//      letterObject[word] = word.length; //creates a property for every word and assigns the length of each word as a value
-    
-// }
-
-
-// console.log('firstLetterArray', firstLetterArray);
-// console.log('letterObject', letterObject);
 
 var master = bcash.hd.fromMnemonic(mnemonic); 
-var hdkey = master.derivePath("m/44'/145'/0'/0");
-var xpubKey  = hdkey.xpubkey();
-var child = hdkey.derive(3, true);
+var hdkey = master.derivePath("m/44'/145'/0/0'/0");
+var xpubKey  = hdkey.xpubkey();//Derive Extended public key
+var child = hdkey.derive(0, false);//Derive a child key from master key
+var ringLegacy = KeyRing.fromPublic(child.publicKey);
+var legAddress = ringLegacy.getAddress('base58', network);//create address from public key
 
+
+let addressArray = [];
+let temp;
+for(let i = 0; i <= 20; i++) {
+    temp = hdkey.derive(i, false);
+    addressArray.push(KeyRing.fromPublic(temp.publicKey).getAddress('base58', network));
+}
+
+// Function creates an object of 20 BCH addresses from Master Public Key
+function addressCollection () {
+    let addressObject = {}
+    let temp;
+    for(let i = 0; i < 20; i++) {
+        //derive child key from index value = to iteration value and temporarily hold in variable
+        temp = hdkey.derive(i, false);
+        //add key name to object with number equal to iterator + 1; add address obtained derived key in temp variable
+        addressObject[`Address ${i + 1}`] = KeyRing.fromPublic(temp.publicKey).getAddress('base58', network)
+    }
+    return addressObject;
+}
 
 
 console.log('Extended Public Key:', xpubKey);
 console.log('Private key of child:', child.privateKey);
+console.log(`BCH Address Array: ${addressArray}`);
+
+console.log(`BCH Address Object: `, addressCollection());
+
+
+
+
+
 
 
 
